a9f498665960c5ea3519fbe99226bf352fd1ebe3
akorop <akorop@ukrcard.com.ua>
2013-01-30 12:19:00 +0200

    Add template method

---
+++ Adapter/Code/Drink.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Adapter.Code
+{
+    public abstract class CaffeineBeverage
+    {
+        public void PrepareRecipe()
+        {
+            BoilWater();
+            Brew();
+            PourInCup();
+            AddCindiments();
+        }
+
+        public abstract void Brew();
+        public abstract  void AddCindiments();
+
+        public void BoilWater()
+        {
+            Console.WriteLine("Boiling water");
+        }
+        public void PourInCup()
+        {
+            Console.WriteLine("Pouring into cup");
+        }
+    }
+    public class Coffee : CaffeineBeverage
+    {
+        public override void Brew()
+        {
+            Console.WriteLine("Dripping Coffee through filter");
+        }
+
+        public override void AddCindiments()
+        {
+            Console.WriteLine("Adding Sugar and Milk");
+        }
+    }
+
+    public class Tea : CaffeineBeverage
+    {
+        public override void Brew()
+        {
+            Console.WriteLine("Steeping the tea");
+        }
+        public override void AddCindiments()
+        {
+            Console.WriteLine("Adding Lemon");
+        }
+    }
+}
---
+++ Adapter/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Adapter.Code;
+
+namespace Adapter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CaffeineBeverage coffee = new Coffee();
+            CaffeineBeverage tea = new Tea();
+
+            coffee.PrepareRecipe();
+            tea.PrepareRecipe();
+
+            Console.ReadKey();
+        }
+    }
+}
